445a964b11b937fc029dd1f4eb79d5331da27a2d
rahulbhati208-maker <rahul.bhati208@gmail.com>
2025-11-19 06:50:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
     cors: { origin: "*" }
 });
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Store connected users for admin view
+const connectedUsers = new Map();
+
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+    connectedUsers.set(socket.id, { id: socket.id, connectedAt: new Date() });
+
+    // Send updated user list to admin when someone connects
+    broadcastUserList();
 
     socket.on("signal", (data) => {
-        socket.broadcast.emit("signal", {
-            id: socket.id,
-            data: data
-        });
+        // If targetId is specified, send only to that user
+        if (data.targetId) {
+            socket.to(data.targetId).emit("signal", {
+                id: socket.id,
+                data: data
+            });
+        } else {
+            // Broadcast to all other users
+            socket.broadcast.emit("signal", {
+                id: socket.id,
+                data: data
+            });
+        }
     });
 
     socket.on("talk-start", () => {
@@ -29,13 +48,31 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         console.log("User left:", socket.id);
+        connectedUsers.delete(socket.id);
         socket.broadcast.emit("user-left", socket.id);
+        broadcastUserList();
     });
 });
 
+function broadcastUserList() {
+    const users = Array.from(connectedUsers.values());
+    io.emit("user-list", users);
+}
+
+// Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'user.html'));
+});
+
+app.get("/admin", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
 // IMPORTANT FOR RENDER
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log("WebSocket server running on port " + PORT);
+    console.log("Server running on port " + PORT);
+    console.log("User page: http://localhost:" + PORT);
+    console.log("Admin page: http://localhost:" + PORT + "/admin");
 });
